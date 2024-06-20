cebef0c00433cf141bc2787234054531bda4be88
Vaishnavi Chemte <126189770+vaishnavinc@users.noreply.github.com>
2024-06-20 23:25:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactive features using JavaScript
+// Example: Add an alert message when the page loads
+window.onload = function() {
+    console.log("Page loaded successfully");
+};
